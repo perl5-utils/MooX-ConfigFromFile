3590c1233616143d5c2079af714b5ac791197e54
Jens Rehsack <sno@netbsd.org>
2015-03-02 12:44:42 +0100

    improve test-coverage

--- t/testlib.pm
+++ t/testlib.pm
@@ -1,6 +1,8 @@
 use strict;
 use warnings FATAL => 'all';
 
+use FindBin;
+
 {
     package    #
       Calc::Role::BinaryOperation;
@@ -64,12 +66,47 @@ use warnings FATAL => 'all';
     }
 }
 
+{
+    package    #
+      Calc::div;
+    use Moo;
+    use MooX::ConfigFromFile;
+
+    with 'Calc::Role::BinaryOperation';
+
+    around BUILDARGS => sub {
+	my $next   = shift;
+	my $class  = shift;
+	my $loaded_config = { a => 12, b => 2 };
+	$class->$next(@_, loaded_config => $loaded_config, b => 3 );
+    };
+
+    sub execute
+    {
+        my $self = shift;
+        return $self->a / $self->b;
+    }
+}
+
+{
+    package
+      Dumb::Cfg;
+
+    use Moo;
+    use MooX::ConfigFromFile;
+
+    sub execute
+    {
+	return;
+    }
+}
+
 my $adder = Calc::add->new( config_prefix => 'calc-operands' );
 ok( defined( $adder->a ), "read 'a' from add config" );
 ok( defined( $adder->b ), "read 'b' from add config" );
 cmp_ok( $adder->execute, "==", 5, "read right adder config" );
 
-my $subber = Calc::sub->new;
+my $subber = Calc::sub->new( config_extensions => $adder->config_extensions, config_dirs => $adder->config_dirs );
 ok( defined( $subber->a ), "read 'a' from sub config" );
 ok( defined( $subber->b ), "read 'b' from sub config" );
 cmp_ok( $subber->execute, "==", -1, "read right subber config" );
@@ -84,3 +121,12 @@ ok( defined( $mul2->a ), "copy 'a' from mul1 config" );
 ok( defined( $mul2->b ), "copy 'b' from mul1 config" );
 cmp_ok( $mul2->execute, "==", 6, "right mul1 config duplicated" );
 
+my $div = Calc::div->new;
+ok( defined( $div->a ), "read 'a' from div config" );
+ok( defined( $div->b ), "read 'b' from div config" );
+cmp_ok( $div->execute, "==", 4, "read right div config" );
+
+my $dumb = Dumb::Cfg->new( config_dirs => 1 );
+isa_ok( $dumb, "Dumb::Cfg" );
+is_deeply( $dumb->config_dirs, [qw(.)], "fallback config dirs" );
+is( $dumb->config_prefix, $FindBin::Script, "fallback config prefix" );
