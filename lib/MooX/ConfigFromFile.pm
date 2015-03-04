a554d3c1bd20d5a075416f516bd34af59e66f87d
Jens Rehsack <sno@netbsd.org>
2015-03-04 13:57:52 +0100

    bump copyright year to 2015

--- lib/MooX/ConfigFromFile.pm
+++ lib/MooX/ConfigFromFile.pm
@@ -201,7 +201,7 @@ L<http://search.cpan.org/dist/MooX-ConfigFromFile/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2013-2014 Jens Rehsack.
+Copyright 2013-2015 Jens Rehsack.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
