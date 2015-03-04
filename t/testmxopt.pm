7ea98a20e08eaf6300407208e5f15c153ea77c50
Jens Rehsack <sno@netbsd.org>
2015-03-04 12:51:11 +0100

    apply Perl::Tidy rules

--- t/testmxopt.pm
+++ t/testmxopt.pm
@@ -93,9 +93,11 @@ EOCDECL
 note $main::OO;
 
 my $cwd = abs_path(getcwd);
-my @cfg_files = map { ("--config-files", File::Spec->catfile($cwd, qw(calc etc), $_) ) } qw(operands.json small-operands.json large-operands.json);
+my @cfg_files = map { ( "--config-files", File::Spec->catfile( $cwd, qw(calc etc), $_ ) ) }
+  qw(operands.json small-operands.json large-operands.json);
 
-SCOPE: {
+SCOPE:
+{
     local @ARGV = qw(--config-prefix calc-operands);
     my $adder = oCalc::add->new_with_options;
     ok( defined( $adder->a ), "read \"a\" from add config" );
@@ -104,7 +106,8 @@ SCOPE: {
     ok( Moo::Role::does_role( $adder, "MooX::ConfigFromFile::Role" ), "Applying MooX::ConfigFromFile::Role to oCalc::add" );
 }
 
-SCOPE: {
+SCOPE:
+{
     local @ARGV = qw(--config-prefix operands);
     my $subber = oCalc::sub->new_with_options;
     ok( defined( $subber->a ), "read \"a\" from sub config" );
@@ -113,34 +116,38 @@ SCOPE: {
     ok( Moo::Role::does_role( $subber, "MooX::ConfigFromFile::Role" ), "Applying MooX::ConfigFromFile::Role to oCalc::sub" );
 }
 
-SCOPE: {
-    local @ARGV = @cfg_files[0..1];
+SCOPE:
+{
+    local @ARGV = @cfg_files[ 0 .. 1 ];
     my $mul = oCalc::mul->new_with_options;
-    is( $mul->a, 21, ,"read \"a\" from mul config" );
-    is( $mul->b, 4, "read \"b\" from mul config" );
+    is( $mul->a, 21,, "read \"a\" from mul config" );
+    is( $mul->b, 4,   "read \"b\" from mul config" );
     cmp_ok( $mul->execute, "==", 84, "read right mul config" );
 }
 
-SCOPE: {
-    local @ARGV = @cfg_files[2..3];
+SCOPE:
+{
+    local @ARGV = @cfg_files[ 2 .. 3 ];
     my $mul = oCalc::mul->new_with_options;
-    is( $mul->a, 21, ,"keep \"a\" from first mul config" );
-    is( $mul->b, 4, "keep \"b\" from first mul config" );
+    is( $mul->a, 21,, "keep \"a\" from first mul config" );
+    is( $mul->b, 4,   "keep \"b\" from first mul config" );
     cmp_ok( $mul->execute, "==", 84, "read right mul config" );
 }
 
-SCOPE: {
-    local @ARGV = @cfg_files[2..3];
+SCOPE:
+{
+    local @ARGV = @cfg_files[ 2 .. 3 ];
     my $div = oCalc::div->new_with_options;
-    is( $div->a, 30, ,"read \"a\" from small div config" );
-    is( $div->b, 6, "read \"b\" from small div config" );
+    is( $div->a, 30,, "read \"a\" from small div config" );
+    is( $div->b, 6,   "read \"b\" from small div config" );
     cmp_ok( $div->execute, "==", 5, "read right div config" );
 }
 
-SCOPE: {
-    local @ARGV = @cfg_files[4..5];
+SCOPE:
+{
+    local @ARGV = @cfg_files[ 4 .. 5 ];
     my $div = oCalc::div->new_with_options;
-    is( $div->a, 666, ,"read \"a\" from large div config" );
-    is( $div->b, 222, "read \"b\" from large div config" );
+    is( $div->a, 666,, "read \"a\" from large div config" );
+    is( $div->b, 222,  "read \"b\" from large div config" );
     cmp_ok( $div->execute, "==", 3, "read right div config" );
 }
